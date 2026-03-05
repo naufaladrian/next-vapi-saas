e07c6e0dbb0d3b35929f67a03c88b094423be07c
naufaladrian <naufal.adrian904@gmail.com>
2026-03-05 15:29:27 +0700

    feat: enhance companions library with search and subject filtering components

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,32 @@
+import CompanionCard from '@/components/CompanionCard';
+import SearchInput from '@/components/SearchInput';
+import SubjectFilter from '@/components/SubjectFilter';
+import { getAllCompanions } from '@/lib/actions/companion.actions';
+import { getSubjectColor } from '@/lib/utils';
 import React from 'react'
 
-const CompanionsLIbrary = () => {
+const CompanionsLIbrary = async ({searchParams}:SearchParams) => {
+    const filters = await searchParams;
+    const subject = filters.subject ? filters.subject :'';
+    const topic = filters.topic ? filters.topic :'';
+
+    const companions = await getAllCompanions({subject,topic})
+
     return (
-        <div>CompanionsLIbrary</div>
+        <main>
+            <section className='flex justify-between gap-4 max-sm:flex-com'>
+                <h1>Companions Library</h1>
+                <div className='flex gap-4'>
+                    <SearchInput/>
+                    <SubjectFilter/>
+                </div>
+            </section>
+            <section className='companions-grid'>
+                {companions.map((c)=>(
+                    <CompanionCard key={c.id} { ...c } color={getSubjectColor(c.subject)} />
+                ))}
+            </section>
+        </main>
     )
 }
 
